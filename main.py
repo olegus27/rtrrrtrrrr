4a98b05eeb5c52366f4bad75aed21fa3e74429ba
olegus27 <116091869+olegus27@users.noreply.github.com>
2022-11-05 15:52:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
-class Cat:
-
-    def __init__(self, name):
+class Human:
+    def __init__(self,name = 'Human'):
         self.name = name
-        self.tasty = 50
-        self.activity = 40
-        self.alive = True
 
-    def to_eat(self):
-        print("Time to eat!")
-        self.tasty += 1
-        self.activity -= 0.5
+class Car:
+    def __init__(self,brand):
+        self.brand = brand
+        self.passangers = []
 
-    def to_sleep(self):
-        print("Time to sleep")
-        self.tasty -= 3
-        self.activity -= 10
+    def add_passangers(self, human):
+        self.passangers.append(human)
 
-    def to_play(self):
-        print("Time to play")
-        self.tasty -= 10
-        self.activity += 11
+    def print_passangers_names(self):
+        if self.passangers != []:
+            print(f'Names of {self.brand} passengers')
+            for passenger in self.passangers:
+                print(passenger.name)
+        else:
+            print(f'There are no passenger in {self.brand}')
 
-    def is_alive(self):
-        if self.tasty < 0:
-            self.alive = False
-        elif self.activity < 0:
-            self.alive = False
+class Company:
+    def __init__(self, brand):
+        self.brand = brand
+        self.employee = []
 
-    def end_day(self):
-        print(f'Tasty = {self.tasty}')
-        print(f'Activity = {self.activity}')
+    def add_employee(self, human):
+        self.employee.append(human)
+
+    def print_employee_name(self):
+        if self.employee != []:
+            print(f'Name of {self.brand} employee')
+            for employee in self.employee:
+                print(employee.name)
+
+        
+
+nick = Human('Nick')
+kate = Human('Kate')
+edison = Human('Edison')
+car = Car('Mercedes')
+company = Company('Mercedes')
+
+car.add_passangers(nick)
+car.add_passangers(kate)
+company.add_employee(edison)
+car.print_passangers_names()
+company.print_employee_name()
